57fbed5f781217fc3f6e06a3154fff53ec407b60
charliebrown1994 <leesungyul@outlook.com>
2025-09-10 19:16:15 +0900

    Feat : 적 만들기 (코드 작성 완료)

---
+++ Assets/Scripts/Enemy/StateMachine/EnemyIdleState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : EnemyBaseState
+{
+    public EnemyIdleState(EnemyStateMachine stateMachine) : base(stateMachine)
+    {
+    }
+
+    public override void Enter()
+    {
+        stateMachine.MovementSpeedModifier = 0f;
+        base.Enter();
+        StartAnimation(stateMachine.Enemy.AnimationData.GroundParameterHash);
+        StartAnimation(stateMachine.Enemy.AnimationData.IdleParameterHash);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+        StopAnimation(stateMachine.Enemy.AnimationData.GroundParameterHash);
+        StopAnimation(stateMachine.Enemy.AnimationData.IdleParameterHash);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        if (IsInChaseRange())
+        {
+            // PlayerWalkState 먼저 생성 후 복귀!
+            stateMachine.ChangesState(stateMachine.ChasingState);
+            return;
+        }
+    }
+}
